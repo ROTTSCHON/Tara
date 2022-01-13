60362fde720b59851ae7f922acd18f8589c41d99
AdiCotirla <adioprea53@gmail.com>
2022-01-13 20:15:17 +0200

    logo deploy

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -5,9 +5,9 @@ import logo from "../../images/logotara.png"
   function Header(){
       return(
         <div className="header">
-            <span><img src={logo} width={"100px"} height={"100px"}/></span>
+            <span><img src={logo} width={"100px"} height={"100px"} alt='logo'/></span>
             <h1>ROTTSCHON KENNEL</h1>
-            <span><img src={logo} width={"100px"} height={"100px"}/></span>
+            <span><img src={logo} width={"100px"} height={"100px"} alt='logo'/></span>
           </div>
       )
   }
