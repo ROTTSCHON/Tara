492c351669a47b8c0bf3cfb6a62d156b080d5231
AdiCotirla <adioprea53@gmail.com>
2022-01-15 20:06:32 +0200

    responsive

--- src/Components/Button/button.js
+++ src/Components/Button/button.js
@@ -1,15 +1,10 @@
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link
-  } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./button.css"
 import logo from "../../images/logotara.png"
   function Buttons()
   {
       return(
-          <div className="container">
+          <div className="container-button-home">
               <div className="upper">
               <h1><span><img src={logo} alt="logo" width={"40px"} height={"40px"}/></span>Romanian Rottweiler Breeder - Rottschon Kennel</h1>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
